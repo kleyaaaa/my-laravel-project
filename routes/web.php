7387b876b19adb2f35be840332c120cabc63f9cf
kleyaaaa <clea.jamila8@gmail.com>
2026-06-05 13:01:47 +0800

    remove updatePassword route

--- routes/web.php
+++ routes/web.php
@@ -51,8 +51,6 @@ Route::get('/profile', [ProfileController::class, 'showProfile'])->name('display
 //Update Profile
 Route::post('/updateProfile', [ProfileController::class, 'update_profile']);
 
-Route::post('/updatePassword', [ProfileController::class, 'update_password']);
-
 //logout
 Route::get('/logout', [LogoutController::class, 'logout']);
 
